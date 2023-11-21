6cd4a4d7e789b23933d3453d41a0832cbb30c625
Bhavya-Choubey <bchoubey2003@gmail.com>
2023-11-21 12:11:44 +0530

    path

--- backend/Database/db.js
+++ backend/Database/db.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 // const mongoURI = process.env.MONGODB_URI;
 
+const cors = require('cors');
+
 const connectToMongo = () => {
   mongoose
     .connect("mongodb+srv://Sneha:Sneha@cluster0.nhxqexb.mongodb.net/test")
@@ -12,5 +14,16 @@ const connectToMongo = () => {
       console.error("Error connecting to MongoDB", error);
     });
 };
+
+
+
+// Allow all origins
+app.use(cors());
+
+// Allow specific origin(s)
+app.use(cors({
+  origin: 'https://mern-school-m2nu.vercel.app/'
+}));
+
 
 module.exports = connectToMongo;
