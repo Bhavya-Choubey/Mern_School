73a579d74287ed99cee886a538cfb5de97efbedb
Bhavya-Choubey <bchoubey2003@gmail.com>
2023-11-21 11:50:18 +0530

    path changed

--- backend/index.js
+++ backend/index.js
@@ -32,5 +32,5 @@ app.get('/', (req, res) => {
 
 })
 app.listen(port, () => {
-  console.log(`Server Listening On http://localhost:${port}`);
+  console.log(`Server Listening On https://mern-school-m2nu.vercel.app/`);
 });
